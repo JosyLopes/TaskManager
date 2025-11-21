006dbc1b1163053b2a0f01e5a6094d776097c1fb
Josy Lopes <59489958+JosyLopes@users.noreply.github.com>
2025-11-21 16:55:38 -0300

    Primeiro

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        Repositorio repo = new Repositorio();
+        Scanner ler = new Scanner(System.in);
+        String acao;
+
+        do {
+            System.out.println("\nComandos: adicionar, listar, buscar, remover, sair");
+            System.out.print("Digite um comando: ");
+            acao = ler.nextLine();
+
+            switch (acao.toLowerCase()) {
+                case "adicionar":
+                    System.out.print("Descrição da tarefa: ");
+                    String descricao = ler.nextLine();
+                    System.out.print("Status da tarefa: ");
+                    String status = ler.nextLine();
+                    repo.adicionarTarefa(descricao, status);
+                    System.out.println("Tarefa adicionada com sucesso.");
+                    break;
+                case "listar":
+                    System.out.println("Lista de Tarefas:");
+                    for (Tarefa tarefa : repo.listarTarefas()) {
+                        System.out.println(tarefa);
+                    }
+                    break;
+                case "buscar":
+                    System.out.print("Descrição para buscar: ");
+                    String buscaDescricao = ler.nextLine();
+                    repo.buscarPorDescricao(buscaDescricao);
+                    break;
+                case "remover":
+                    System.out.print("Id da tarefa que deseja remover: ");
+                    int removerID = ler.nextInt();
+                    repo.removerTarefa(removerID);
+                    break;
+                case "sair":
+                    System.out.println("Saindo do programa.");
+                    break;
+                default:
+                    System.out.println("Comando inválido. Tente novamente.");
+            }
+        } while (!acao.equalsIgnoreCase("sair"));
+
+        ler.close();
+    }
+}
---
+++ src/Repositorio.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Repositorio {
+    private ArrayList<Tarefa> tarefas;
+    private int contadorId;
+
+    public Repositorio(){
+        this.tarefas = new ArrayList<>();
+        this.contadorId = 1;
+    }
+
+    //metodo para adicionar tarefa
+    public void adicionarTarefa(String descricao, String status) {
+        Tarefa novaTarefa = new Tarefa(contadorId++, descricao, status);
+        tarefas.add(novaTarefa);
+    }
+
+    // Listar todas as tarefas
+    public ArrayList<Tarefa> listarTarefas() {
+        return tarefas;
+    }
+
+    // Buscar tarefa por descrição
+    public void buscarPorDescricao(String descricao) {
+            boolean encontrou = false;
+            for (Tarefa tarefa : tarefas) {
+                if (tarefa.getDescricao().toLowerCase().contains(descricao.toLowerCase())) {
+                    System.out.println(tarefa);
+                    encontrou = true;
+                }
+            }
+            if (!encontrou) {
+                System.out.println("Nenhuma tarefa encontrada com a descrição: " + descricao);
+        }
+
+    }
+
+    // Remover tarefa por ID
+    public void removerTarefa(int idTarefa) {
+        Scanner ler = new Scanner(System.in);
+        for (int i = 0; i < tarefas.size(); i++) {
+            if (tarefas.get(i).getId() == idTarefa) {
+                System.out.print("Tem certeza que deseja remover a tarefa com ID: " + idTarefa + " E Descricao: " + tarefas.get(i).getDescricao() + "? (s/n): ");
+                String confirmacao = ler.nextLine();
+                if (confirmacao.equalsIgnoreCase("s")) {
+                    tarefas.remove(i);
+                    System.out.println("Tarefa removida com sucesso.");
+                } else {
+                    System.out.println("Remoção cancelada.");
+                }
+                return;
+            }
+        }
+        System.out.println("Tarefa com ID " + idTarefa + " não encontrada.");
+    }
+
+
+
+}
